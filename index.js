ceb77d1ab558f18208ebf99d8d52ee5814a1c4ab
prakash <suryaprakashk747@gmail.com>
2024-09-15 11:56:12 +0530

    fist commit
    Installed basic node modules: express, ejs, body-parser, axios

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+const port = 3000;
+
