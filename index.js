1777ce8602f93bc71d38f6641591afdfb0f537c1
prakash <suryaprakashk747@gmail.com>
2024-09-15 13:05:26 +0530

    check get route
    Tried to check if everything is fine up to this point. Checked the get route using gautam's name.

--- index.js
+++ index.js
@@ -6,5 +6,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const name1 = "Sai Gautam";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {name:name1});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
 
